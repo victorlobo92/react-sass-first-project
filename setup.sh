503ebbdfd54878b1b24242be6bbc719c303ed440
Victor Lobo <victorlobo@ciandt.com>
2023-02-24 17:06:17 -0300

    Update setup.sh to create new react app with vite

--- setup.sh
+++ setup.sh
@@ -8,13 +8,19 @@ else
   if test ! -f "/app/package.json"; then
     echo "Creating a new react app..."
 
-    npx create-react-app .
+    npm create vite@latest . -- --template react
 
     chown -R "${UID}":"${GID}" ./
   else
     echo "Application already exists!"
   fi
 
-  echo "Starting application..."
-  npm start
+  echo "Installing react dependencies..." &
+  npm install
+
+  echo "Installing sass package..." &
+  npm install -D sass
+
+  echo "Starting application..." &
+  npm run dev -- --host --port 3000
 fi
